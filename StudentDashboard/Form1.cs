3c4c07e6b692b5285458e7d7d4279dc7a50280db
ataerena <ataerena599@gmail.com>
2024-03-15 00:08:46 +0300

    Deneme

--- StudentDashboard/Form1.cs
+++ StudentDashboard/Form1.cs
@@ -14,5 +14,13 @@ namespace StudentDashboard
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /*DENEME*/
+
     }
 }
