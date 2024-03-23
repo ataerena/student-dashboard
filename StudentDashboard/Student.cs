0dc1c6ecb75486f62e32bf4579a5789bb6a29f94
Erkincetinkaya <54685384+Erkincetinkaya@users.noreply.github.com>
2024-03-23 20:08:14 +0300

    Student Class implemented

---
+++ StudentDashboard/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentDashboard
+{
+   public class Student
+    {
+        public int id { get; set; }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public int StudentId { get; set; }
+
+        public string Password { get; set; }
+
+        public int Semester { get; set; }
+        public Student() 
+        {
+        }
+
+
+    }
+}
