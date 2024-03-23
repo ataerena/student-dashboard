bf24dfe1d3e5defe04933f11aa0b61a198b4b8cd
ataerena <ataerena599@gmail.com>
2024-03-23 20:06:38 +0300

    Login UI

--- StudentDashboard/Form1.cs
+++ StudentDashboard/Form1.cs
@@ -15,6 +15,31 @@ namespace StudentDashboard
             InitializeComponent();
         }
 
+        private void StudentID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
         /*ATA DENEME 2*/
     }
 }
